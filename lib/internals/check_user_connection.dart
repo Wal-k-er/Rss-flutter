4b7c59db7e5344f64a5f216f154c31abbdd86755
Wal-k-er <pavelbogdanov807@gmail.com>
2023-03-04 22:02:13 +0300

    logs deleted

--- lib/internals/check_user_connection.dart
+++ lib/internals/check_user_connection.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'dart:io';
 
 bool ActiveConnection = false;
@@ -7,15 +6,9 @@ Future CheckUserConnection() async {
   try {
     final result = await InternetAddress.lookup('google.com');
     if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-
-        log('aaaaaaaaa');
         ActiveConnection = true;
-
     }
   } on SocketException catch (_) {
-
-      log('______');
       ActiveConnection = false;
-
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,3 @@
-import 'dart:developer';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:habr_rss/internals/check_user_connection.dart';
 import 'package:webfeed/webfeed.dart';
